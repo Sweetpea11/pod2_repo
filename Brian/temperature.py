63257e4c32ea451cfbcaac3b2497d37e07f73e7f
sterno2510 <sterno2510@gmail.com>
2021-09-16 20:20:39 -0400

    Uploading Challenge #1

---
+++ Brian/temperature.py
@@ -0,0 +1,21 @@
+# Convert 100 degrees F to C
+celsius_100 = (100-32) * (5/9)
+print(celsius_100)
+
+# Checking to see what type variable is
+print(type(celsius_100))
+
+# Convert 0 degrees F to C
+celsius_0 = (-32) * (5/9)
+print(celsius_0)
+
+# Convert 34.2 degrees F to C
+print((34.2-32) * (5/9))
+
+# Convert 5 degrees C to F
+print((9/5) * 5 + 32)
+
+# Convert 30.2 degrees C to F
+print((9/5) * 30.2 + 32)
+
+# Because the output was 86.36 F, 30.2 degrees C is hotter than 85.1 F
