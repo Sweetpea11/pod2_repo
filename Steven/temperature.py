20b1aade4d14d23b42ca11a78476c3a8737d4c2f
stevenkennedyonline <kennsi@me.com>
2021-09-16 20:55:03 -0400

    adding temperature.py challenge

---
+++ Steven/temperature.py
@@ -0,0 +1,29 @@
+# Convert a temperature of 100 degrees fahrenheit to celsius
+
+celsius_100 = (100-32)*5/9
+print(celsius_100)
+
+# Convert a temperature of 0 degrees fahrenheit to celsius
+
+celsius_0 = (0-32)*5/9
+print(celsius_0)
+
+# Convert a temperature of 34.2 degrees fahrenheit to celsius
+
+print(34.2-32)*5/9
+
+# Convert a temperature of 5 degrees celsius to fahrenheit?
+
+print(5*9/5)+32
+
+# What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
+
+f_30_point_2 = (30.2*9/5)+32
+f_30_point_2 = (85.1-32)*5/9
+
+# print out the result
+print(f_30_point_2)
+
+# 30.2 degrees is hotter than 85.1 degrees fahrenheit.
+
+# If you haven't already, go through your script and add a comment for questions 1-5 explaining what your code is doing.
