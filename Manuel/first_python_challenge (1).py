0835fbc80b1f94f5db54a9706c38d47169b804ad
manny2trill <manuelr42787@gmail.com>
2021-09-21 11:06:10 -0500

    more challenges

---
+++ Manuel/first_python_challenge (1).py
@@ -0,0 +1,40 @@
+# print('1: Describe what is happening below by adding comments before each line')
+# variables are being organized
+box_1 = 'books'
+print(box_1)
+box_2 = 'clothes'
+print(box_2)
+box_3 = 'plants'
+print(box_3)
+box_4 = 'kitchen stuff'
+print(box_4)
+
+# print('2: Print the variables box_1, box_2, box_3, box_4')
+
+# print('3.1: Declare a variable with the name "address", assign it any street name you like')
+
+address = 'Crenshaw'
+
+# print('3.2: Print the address variable')
+
+print(address)
+
+# print('4: Reassign variables box_2 and box_4 with some other text and print box_1, box_2, box_3, box_4 again')
+
+box_2 = 'shoes'
+box_4 = 'bedroom stuff'
+print(box_1)
+print(box_2)
+print(box_3)
+print(box_4)
+
+# print('5.1: The line of code below is commented out because it produces many SyntaxErrors. Fix the problem and turn the comment back into regular Python code')
+
+completion_message = 'Completed the first Python challenge!'
+
+# print('5.2: Turn the comment below back into regular Python code')
+
+print(completion_message)
+
+#I had fun doing this!
+
