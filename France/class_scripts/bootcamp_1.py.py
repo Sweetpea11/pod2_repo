bf5f6b4327b612958d0aaaa455ac27930a18dbb0
yahighness <franceshameeka@gmail.com>
2021-09-22 22:40:53 -0400

    complete bootcamp

---
+++ France/class_scripts/bootcamp_1.py.py
@@ -0,0 +1,10 @@
+# this is my first print statement
+print ('hi this is your computer')
+# this is my second print statement
+print ('I can run alot of print functions')
+firstname = "shameeka"
+print(firstname)
+firstname = "shameeka"
+print(firstname)
+full_name = "shameeka_france"
+print(full_name)
