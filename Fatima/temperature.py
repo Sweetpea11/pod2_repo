37f9b50fcce25f31c459862e4750d4758b2846a7
timamonteiro <timamonteiro@icloud.com>
2021-09-25 13:51:09 -0500

    temp challenge

---
+++ Fatima/temperature.py
@@ -0,0 +1,29 @@
+# convert a temp of 100 degrees farenheit to celsius
+
+
+celsius_85 = (85.1-32)*(5/9)
+print(celsius_85)
+
+# Convert a temperature of 0 degrees fahrenheit to celsius
+
+celsius_0 = (0-32)*5/9
+print(celsius_0)
+
+# Convert a temperature of 34.2 degrees fahrenheit to celsius34
+print(34.2-32)*5/9
+
+# Convert a temperature of 5 degrees celsius to fahrenheit?
+
+print(5*9/5)+32
+
+# What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
+
+
+f_30_point_2 = (30.2*9/5)+32
+
+# If you haven't already, go through your script and add a comment for questions 1-5 explaining what your code is doing.
+
+celsius_100 = (85.1-32) * (5/9)
+print(celsius_100)
+
+print(30.2*9/5)+32
