7316eed329fe7c847ba94d5b5b71708577cf248e
timamonteiro <timamonteiro@icloud.com>
2021-09-26 21:01:59 -0500

    snippets_challenge.py

--- Fatima/snippets_challenge.py
+++ Fatima/snippets_challenge.py
@@ -9,9 +9,9 @@ u = 5
 v = 2
 
 if u * v == 10:
-print({f"The product of u {u} and v {v} is 10"})
+print(f"The product of u {u} and v {v} is 10")
 
-else:
+else:gi
 print(f"The product of u ({u}) and v ({v}) is not 10")
 print{f'The product of u){u}) and v ({v}) is not 10'}
 print()
