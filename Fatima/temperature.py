9a1878c00713d4527cce8a19c4160ac64099baef
timamonteiro <timamonteiro@icloud.com>
2021-10-07 09:23:26 -0500

    temperature.py

--- Fatima/temperature.py
+++ Fatima/temperature.py
@@ -10,20 +10,28 @@ celsius_0 = (0-32)*5/9
 print(celsius_0)
 
 # Convert a temperature of 34.2 degrees fahrenheit to celsius34
-print(34.2-32)*5/9
+
+
+celsius_34 = (34.2-32)*(5/9)
+print(celsius_34)
 
 # Convert a temperature of 5 degrees celsius to fahrenheit?
 
-print(5*9/5)+32
+celsius_5 = 5
+
+fahrenheit = (celsius_5 * 1.8) + 32
+
+print(fahrenheit)
 
 # What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
 
 
 f_30_point_2 = (30.2*9/5)+32
 
+print(f_30_point_2)
+
 # If you haven't already, go through your script and add a comment for questions 1-5 explaining what your code is doing.
 
 celsius_100 = (85.1-32) * (5/9)
 print(celsius_100)
 
-print(30.2*9/5)+32
