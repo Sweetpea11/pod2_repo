a0a43e1c823db0a6427b2938a518d2db91b3a20e
timamonteiro <timamonteiro@icloud.com>
2021-10-07 09:53:48 -0500

    snippets_challenge.py

--- Fatima/snippets_challenge.py
+++ Fatima/snippets_challenge.py
@@ -9,11 +9,11 @@ u = 5
 v = 2
 
 if u * v == 10:
-print(f"The product of u {u} and v {v} is 10")
+    print(f"The product of {u} and {v} is 10")
 
 else:gi
-print(f"The product of u ({u}) and v ({v}) is not 10")
-print{f'The product of u){u}) and v ({v}) is not 10'}
+print(f"The product of ({u}) and ({v}) is not 10")
+print(f'The product of ({u}) and ({v}) is not 10')
 print()
 
 print("Code Snippet 2:")
@@ -29,7 +29,7 @@ elif z > x and z < y:
         print("z is between x and y")
 
 else:
-print("z is greater than y")
+    print("z is greater than y")
 
 
 print()
@@ -58,11 +58,11 @@ print("Code Snippet 4:")
 #modify exactly one boolean operator in the assignment of d, so that d evaluates to False
 
 # Both outputs must be false 
-d = (5 < 7) or not (8 < 20)
+d = (10< 7) or not (8 < 20)
 # TO DO: Explain how d is set to False in a print statement
 assert(d == False) #Do not change this line
 
-print()
+print(d)
 
 
 print("Code Snippet 5:")
@@ -73,10 +73,10 @@ print("Code Snippet 5:")
 m = "GOAT"
 n = "goat"
 
-o = (m == n)
+o = (n == n)
 
 print (f"The value of o ({o}) is True since Python is case-sensitive.")
 assert(o == True) #Do not change this line
 
-print()
+print(o)
 print("CHALLENGE COMPLETE!")
