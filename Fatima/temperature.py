c429a0e94f3db0379303277c4ab3ca6e0de0616e
timamonteiro <timamonteiro@icloud.com>
2021-10-10 17:44:38 -0500

    add

--- Fatima/temperature.py
+++ Fatima/temperature.py
@@ -1,37 +1,41 @@
-# convert a temp of 100 degrees farenheit to celsius
+print('Question 1:')
+# convert 100 degrees f to celsius
+celsius_100 = (100-32)*5/9
+# print out the result
+print(celsius_100) 
+# print out that it is a float
+print('It is a float! There are decimal points')
 
 
-celsius_85 = (85.1-32)*(5/9)
-print(celsius_85)
 
-# Convert a temperature of 0 degrees fahrenheit to celsius
-
+print('Question 2:')
+# convert 0 degrees f to celsius
 celsius_0 = (0-32)*5/9
+# print out result
 print(celsius_0)
-
-# Convert a temperature of 34.2 degrees fahrenheit to celsius34
-
-
-celsius_34 = (34.2-32)*(5/9)
-print(celsius_34)
-
-# Convert a temperature of 5 degrees celsius to fahrenheit?
-
-celsius_5 = 5
-
-fahrenheit = (celsius_5 * 1.8) + 32
-
-print(fahrenheit)
-
-# What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
+# print out that it is a float 
+print('It is a float! There are decimal points')
 
 
+
+print('Question 3:')
+# print out 34.2 degrees f converted to celsius
+print((34.2-32)*5/9)
+
+
+
+print('Question 4:')
+# reverse the steps! first multiple by 9/5, then add 32
+print((5*9/5)+32)
+
+
+
+print('Question 5:')
+# first convert 30.2 celsius to fahrenheit
 f_30_point_2 = (30.2*9/5)+32
 
+# print out the result
 print(f_30_point_2)
 
-# If you haven't already, go through your script and add a comment for questions 1-5 explaining what your code is doing.
-
-celsius_100 = (85.1-32) * (5/9)
-print(celsius_100)
-
+# compare the values
+print(f'30.2 degrees celsius is equal to {f_30_point_2} degrees fahrenheit, so it hotter than 85.1 degrees fahrenheit!')
