6cb066c863c599e68d9f26897de627db54cd6ec1
manny2trill <manuelr42787@gmail.com>
2021-10-14 14:57:27 -0500

    unfinished challenges i need to finish

---
+++ Manuel/Challenges/book_class.py
@@ -0,0 +1,18 @@
+class Booklist():
+	def __init__(self):
+		self.books = []
+	
+	def add(self, title, author):
+    		
+		book.['title'] = title
+		book.['author'] = author
+
+	def count_books(self):
+		pass
+
+	def remove_title(self, title):
+		pass
+
+	def display_titles(self):
+		pass
+
