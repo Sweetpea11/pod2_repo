605a7180acdc1efcda28c6826a1e5b066dc7af82
sterno2510 <sterno2510@gmail.com>
2021-10-15 21:48:11 -0400

    Created the stations challenge

---
+++ Brian/station_planning.py
@@ -0,0 +1,12 @@
+from stations_challenge import *
+
+bus_station2 = BusStation(station_name='Brian S. Bus Depot', location = '5 Madison Ave', routes = ['Denver', 'Atlantic City', 'Buffalo'])
+
+station3 = SubwayStation(station_name='BoBo', location='60 Greene St', lines=['X', 'Z', 'P', 'Q'])
+
+station4 = SubwayStation(station_name='Alexander', location='Austin Ave', lines=['AAA', 'BBB', 'CCC ', 'DDD'])
+
+bus_station2.show_info()
+station3.show_info()
+station4.show_info()
+
