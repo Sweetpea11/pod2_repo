0a350da57d6afe858d36769c117a5fde3db41392
jankyman <ehonda.punch@gmail.com>
2021-10-18 14:25:45 -0400

    adding challenges

---
+++ Christian/book_class.py
@@ -0,0 +1,17 @@
+class Booklist():
+	def __init__(self,):
+		self.books =[]
+
+
+	def add(self, title, author):
+		self.books.append ({'title':title,'author':author})
+	def count_books(self):
+		return len(self.books)
+
+	def remove_title(self, title):
+		for book in self.books:
+			if title == book['title']:
+				self.books.remove(book)
+
+	def display_titles(self):
+		pass
---
+++ Christian/station_planning.py
@@ -0,0 +1,3 @@
+import stations_challenge as sc 
+
+dollar_bus = sc.BusStation(location='The loop', )
