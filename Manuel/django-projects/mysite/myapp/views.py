838abfb7498152d6f0c337c9f36a866786e7bf0a
manny2trill <manuelr42787@gmail.com>
2021-10-25 19:37:36 -0500

    starting over

--- Manuel/django-projects/mysite/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def hello(request, name='world'):
-    if request.method == 'GET':
-        return render(request, 'hello.html', { 'name': name })
-
-        
-
