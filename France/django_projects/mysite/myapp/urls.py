20d4cdd9969085838c1d6364ab2583001f2e59cc
yahighness <franceshameeka@gmail.com>
2021-10-27 01:18:55 -0400

    this is the good bye app

---
+++ France/django_projects/mysite/myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from myapp.views import hello, goodbye
+
+urlpatterns = [ 
+    path('', hello, name='hello'),
+    path('<str:name>', hello, name='hello_name'),
+    path('goodbye/<str:name>', goodbye, name='goodbye_name')
+]
+
+
